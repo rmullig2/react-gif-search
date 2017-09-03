19de7233a6c4ec526d0ecc2e4bb2c983a5736087
Ray Mulligan <rmullig2@optonline.net>
2017-09-02 20:05:27 -0400

    Part 3 Complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from 'redux';
 import GifsReducer from './gifs';
 import ModalReducer from './modal';
+import AuthReducer from './auth';
+import { routerReducer } from 'react-router-redux';
+import { reducer as FormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
     gifs: GifsReducer,
-    modal: ModalReducer
+    modal: ModalReducer,
+    auth: AuthReducer,
+    router: routerReducer,
+    form: FormReducer
 })
 
 export default rootReducer;
